2e808d862c436b7836703b47823f9fe33897514a
Ege Arslan <108265231+ArslanEge@users.noreply.github.com>
2023-05-05 15:14:46 +0300

    Update arduino.py

--- ardunio-backend/arduino.py
+++ ardunio-backend/arduino.py
@@ -16,6 +16,32 @@ ard=APIRouter(
     tags=["arduino"]
 )
 
+def get_current_date_time():
+    # set the timezone to 'Europe/Istanbul'
+    tz = pytz.timezone('Europe/Istanbul')
+
+    # get the current time in Istanbul's local time
+    now = datetime.datetime.now(tz)
+
+    # extract the month, day, hour, and minute from the current time
+    current_month = now.month
+    current_day = now.day
+    current_hour = now.hour
+    current_minute = now.minute
+
+    # convert the values to strings
+    current_month_str = str(current_month).zfill(2) # adds a leading zero if necessary
+    current_day_str = str(current_day).zfill(2)
+    current_hour_str = str(current_hour).zfill(2)
+    current_minute_str = str(current_minute).zfill(2)
+
+    # create a date/time string in the format: "MM/DD HH:MM"
+    date_time = current_month_str + '/' + current_day_str + ' ' + current_hour_str + ':' + current_minute_str
+
+    # return the date/time string
+    return date_time
+
+
 @ard.post("/heat")
 async def add_heat(value:models.Arduino):
   
@@ -25,17 +51,12 @@ async def add_heat(value:models.Arduino):
 
   return {"SELAM"}
 
+
+
 @ard.get("/get")
 async def get_heat(temperature:str,humidity:str):
         url = 'https://fastapi-arduino.herokuapp.com/arduino/heat'
-        tz = pytz.timezone('Europe/Istanbul')
-        now = datetime.datetime.now(tz)
-        current_month = str(now.month)
-        current_day = str(now.day)
-        current_hour = str(now.hour)
-        current_minute = str(now.minute)
-
-        date_time = str(current_month+"/"+current_day+"/"+current_hour+"/"+current_minute)
+        
 
         # create the data to be sent in the HTTP POST request
         tvalue=str(temperature)
@@ -43,7 +64,7 @@ async def get_heat(temperature:str,humidity:str):
         data = {
             'temperature': tvalue,
             'humidity': hvalue,
-            'date_time': date_time
+            'date_time': get_current_date_time()
         }
         header = {
       'Content-Type': 'application/json',

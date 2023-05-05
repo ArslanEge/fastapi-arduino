64b075e333162da3ff3a017353000fc56cb7d7ec
Ege Arslan <108265231+ArslanEge@users.noreply.github.com>
2023-05-06 00:59:34 +0300

    Update main.py

--- ardunio-backend/main.py
+++ ardunio-backend/main.py
@@ -18,19 +18,7 @@ app.include_router(ard)
 app.include_router(flutter)
 
 
-origins = [
-   "http:// 192.168.0.11.:8000",
-   "http://localhost",
-   "http://localhost:8080",
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 

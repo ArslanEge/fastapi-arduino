d6b1ca6cfdc5c3d991ceeded04e5b26b90130615
Ege Arslan <108265231+ArslanEge@users.noreply.github.com>
2023-05-05 23:41:13 +0300

    Update main.py

--- ardunio-backend/main.py
+++ ardunio-backend/main.py
@@ -4,13 +4,30 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from user import user
-from arduino import ard
+from arduino import ard,flu
 import models
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
 app.include_router(user)
 app.include_router(ard)
+app.include_router(flu)
+
+origins = [
+   "http:// 192.168.0.11.:8000",
+   "http://localhost",
+   "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
